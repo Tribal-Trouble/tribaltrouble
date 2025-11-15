cc19c17fd3792a19b0d27b63e950ea826d5617ec
Mike Duigou <bondolo@github.com>
2025-11-15 15:51:52 -0800

    modernize GeneratorClouds

--- tt/src/main/java/com/oddlabs/tt/procedural/GeneratorClouds.java
+++ tt/src/main/java/com/oddlabs/tt/procedural/GeneratorClouds.java
@@ -8,47 +8,44 @@ import com.oddlabs.tt.resource.GLIntImage;
 import org.jspecify.annotations.NonNull;
 import org.lwjgl.opengl.GL11;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntSupplier;
+import java.util.stream.Stream;
+
 public final class GeneratorClouds extends TextureGenerator {
 	private static final int TEXTURE_SIZE = 256;
 
 	public static final int INNER = 0;
 	public static final int OUTER = 1;
 
-	private final Landscape.TerrainType terrain;
+	private final Landscape.@NonNull TerrainType terrain;
 
-	public GeneratorClouds(Landscape.TerrainType terrain) {
+	public GeneratorClouds(Landscape.@NonNull TerrainType terrain) {
 		this.terrain = terrain;
 	}
 
 	@Override
-	public Texture @NonNull [] generate() {
-		int seed = Globals.LANDSCAPE_SEED;
-		Channel clouds1 = new Midpoint(TEXTURE_SIZE, 3, 0.55f, seed).toChannel();
-		Channel clouds2 = new Midpoint(TEXTURE_SIZE, 2, 0.4f, seed).toChannel();
+    public Texture @NonNull [] generate() {
+        int seed = Globals.LANDSCAPE_SEED;
+        Channel clouds1 = new Midpoint(TEXTURE_SIZE, 3, 0.55f, seed).toChannel();
+        Channel clouds2 = new Midpoint(TEXTURE_SIZE, 2, 0.4f, seed).toChannel();
 
-		switch (terrain) {
-			case NATIVE:
-				clouds1.dynamicRange(0.5f, 1f, 0f, 1f).gamma(0.75f).brightness(0.5f);
-				clouds2.dynamicRange(0.25f, 1f, 0f, 1f).gamma(0.5f).brightness(0.33f);
-				break;
-			case VIKING:
-				clouds1.dynamicRange(0.5f, 1f, 0f, 0.75f);
-				clouds2.dynamicRange(0.5f, 1f, 0f, 0.75f);
-				break;
-			default:
-				assert false : "illegal terrain";
-				break;
-		}
-
-		if (Landscape.DEBUG) new GLIntImage(clouds1.toLayer()).saveAsPNG("generator_clouds_1");
-		if (Landscape.DEBUG) new GLIntImage(clouds2.toLayer()).saveAsPNG("generator_clouds_2");
-		GLByteImage[] cloud_images = new GLByteImage[] {new GLByteImage(clouds1, GL11.GL_LUMINANCE), new GLByteImage(clouds2, GL11.GL_LUMINANCE)};
-		Texture[] textures = new Texture[cloud_images.length];
-		for (int i = 0; i < cloud_images.length; i++) {
-			textures[i] = new Texture(cloud_images[i].createMipMaps(), Globals.COMPRESSED_LUMINANCE_FORMAT, GL11.GL_LINEAR_MIPMAP_LINEAR, GL11.GL_LINEAR, GL11.GL_REPEAT, GL11.GL_REPEAT);
-		}
-		return textures;
-	}
+        IntSupplier debugImageCount = (new AtomicInteger())::incrementAndGet;
+        return Stream.of(terrain)
+            .flatMap(terrainType -> switch (terrainType) {
+                case NATIVE ->  Stream.of(
+                        clouds1.dynamicRange(0.5f, 1f, 0f, 1f).gamma(0.75f).brightness(0.5f),
+                        clouds2.dynamicRange(0.25f, 1f, 0f, 1f).gamma(0.5f).brightness(0.33f));
+                case VIKING -> Stream.of(
+                        clouds1.dynamicRange(0.5f, 1f, 0f, 0.75f),
+                        clouds2.dynamicRange(0.5f, 1f, 0f, 0.75f));
+            }).peek(img -> {
+                if (Landscape.DEBUG) new GLIntImage(img.toLayer()).saveAsPNG("generator_clouds_" + debugImageCount.getAsInt());
+            }).map(cloud -> new GLByteImage(cloud, GL11.GL_LUMINANCE))
+            .map(GLByteImage::createMipMaps)
+            .map(images -> new Texture(images, Globals.COMPRESSED_LUMINANCE_FORMAT, GL11.GL_LINEAR_MIPMAP_LINEAR, GL11.GL_LINEAR, GL11.GL_REPEAT, GL11.GL_REPEAT))
+            .toArray(Texture[]::new);
+    }
 
 	@Override
 	public int hashCode() {
