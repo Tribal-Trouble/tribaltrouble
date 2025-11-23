cc77ffc790ca26dbbf22edd6750457b5bb72c06a
Mike Duigou <bondolo@github.com>
2025-11-23 15:12:52 -0800

    modernization

--- tt/src/main/java/com/oddlabs/tt/model/SupplyManagers.java
+++ tt/src/main/java/com/oddlabs/tt/model/SupplyManagers.java
@@ -3,25 +3,27 @@ package com.oddlabs.tt.model;
 import com.oddlabs.tt.landscape.TreeSupply;
 import com.oddlabs.tt.landscape.World;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class SupplyManagers {
-	private final Map<Class<? extends Supply>,SupplyManager> supply_managers = new HashMap<>();
+	private final @NonNull Map<@NonNull Class<? extends Supply>, @NonNull SupplyManager> supply_managers;
 
 	public final void debugSpawn() {
             supply_managers.values().forEach(SupplyManager::debugSpawnSupply);
 	}
 
 	public SupplyManagers(@NonNull World world) {
-		supply_managers.put(TreeSupply.class, new SupplyManager(world));
-		supply_managers.put(RockSupply.class, new SupplyManager(world));
-		supply_managers.put(IronSupply.class, new SupplyManager(world));
-		supply_managers.put(RubberSupply.class, new RubberSupplyManager(world));
+		supply_managers = Map.of(
+                TreeSupply.class, new SupplyManager(world),
+                RockSupply.class, new SupplyManager(world),
+                IronSupply.class, new SupplyManager(world),
+                RubberSupply.class, new RubberSupplyManager(world)
+        );
 	}
 
-	public final SupplyManager getSupplyManager(Class<? extends Supply> type) {
+	public final @Nullable SupplyManager getSupplyManager(@NonNull Class<? extends Supply> type) {
 		return supply_managers.get(type);
 	}
 }
