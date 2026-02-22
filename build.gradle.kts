1866916a58a001c09db6e4593599546aae5e737b
RcubDev <rlinehan2@gmail.com>
2026-02-21 18:15:18 -0700

    setup for gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    java
+}
+
+allprojects {
+    group = "com.oddlabs.tribaltrouble"
+    version = "1.0"
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "tribaltrouble"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+include("common", "tools", "tt", "server")
